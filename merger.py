ade4c32e72e556b69f48e09d230a0fe9088a478c
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-01-04 15:43:09 +0100

    Provided an explanation for the choice of data structure. Created the adjacency map that will be used by merger.py and the AST analyser.

--- merger.py
+++ merger.py
@@ -2,10 +2,36 @@ import sys
 import os
 import json
 from cyclonedx.model.bom import Bom
-import tomllib
+from dataclasses import dataclass
+from sortedcontainers import SortedSet
+from typing import Any, Self
 
 DS1_PATH = os.path.join(".", "ds1")
 DS2_PATH = os.path.join(".", "ds2")
+
+@dataclass
+class Dependency:
+    """
+    Docstring for Dependency
+    
+    
+    """
+    id: int
+    name: str
+    children: list[Self] | None 
+
+@dataclass
+class PackageComparisonEntry:
+    """
+    Allows to draw an easy comparison between approaches
+
+    name: package's name
+    main_source: path to the main source to build a dependency tree from. can be a list in case the package is a framework and has thus no main starting file
+    """
+    name: str
+    main_source: str | list[str] 
+    metadata: str
+    deps_gathered: dict[str, list[Dependency]] # str is the tool's name (e.g syft, cdxgen) and the value 
 
 def generate_template_ds1():
     def read_metadata(src: str) -> list[str]:
@@ -44,15 +70,23 @@ ds2_template = {
 }
 
 def import_ds1_sboms(src: str):
+    def extract_dependencies(level: int, dependencies: SortedSet) -> dict:
+        deps_dict = {}
+
+        for dependency in dependencies:
+            
+            extract_dependencies(level=level+1, dependency.dependencies)
+
     tools = os.listdir(src)
     for tool in tools:
         packages = os.listdir(os.path.join(src, tool))
 
         for package in packages:
             with open(os.path.join(src, tool, package)) as input_json:
-                deserialized_bom = Bom.from_json(data=json.loads(input_json.read()))
+                deserialized_bom = Bom.from_json(data=json.loads(input_json.read())) # type: ignore
 
-                print(deserialized_bom)
+                
+                    
     
 generate_template_ds1()
 import_ds1_sboms(src=os.path.join(DS1_PATH, "sbom"))
